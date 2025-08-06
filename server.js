9fae4287f1a8206cd95865a8777dbbd443998c0b
Cesar <csarfau@gmail.com>
2025-08-06 00:22:42 -0300

    refactor: remove swagger

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { agentesRouter } from './routes/agentesRoutes.js';
 import { casosRouter } from './routes/casosRoutes.js';
 import { errorHandler } from './utils/errorHandler.js';
-import { setupSwagger } from './docs/swagger.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,9 +13,6 @@ app.use('/casos', casosRouter);
 
 app.use(errorHandler);
 
-setupSwagger(app);
-
 app.listen(PORT, () => {
   console.log(`Servidor do Departamento de Polícia rodando em: http://localhost:${PORT}`);
-  console.log(`Swagger: http://localhost:${PORT}/docs`);
 });
