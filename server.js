b1b5b42c1f1e23a00c3b36fe07ff0e85e6a5fd9a
Cesar <csarfau@gmail.com>
2025-08-09 00:51:55 -0300

    fix: change format date returns

--- server.js
+++ server.js
@@ -2,9 +2,31 @@ import express from 'express';
 import { agentesRouter } from './routes/agentesRoutes.js';
 import { casosRouter } from './routes/casosRoutes.js';
 import { errorHandler } from './utils/errorHandler.js';
+import dayjs from 'dayjs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+function formatDates(obj) {
+  if (obj instanceof Date) {
+    return dayjs(obj).format('YYYY-MM-DD');
+  }
+  if (Array.isArray(obj)) {
+    return obj.map(formatDates);
+  }
+  if (obj && typeof obj === 'object') {
+    return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, formatDates(v)]));
+  }
+  return obj;
+}
+
+app.use(function (req, res, next) {
+  const oldJson = res.json;
+  res.json = function (data) {
+    return oldJson.call(this, formatDates(data));
+  };
+  next();
+});
 
 app.use(express.json());
 
